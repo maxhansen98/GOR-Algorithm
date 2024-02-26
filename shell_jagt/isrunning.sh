892f0e08a048f9cd5b4fc11444e273ed950aab2a
Malte Weyrich <malte.weyrich@hotmail.de>
2024-02-26 18:53:25 +0100

    fixed isrunning

---
+++ shell_jagt/isrunning.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/bash 
+
+# get input
+proc=$1
+usr=$2
+
+# top prints out one refresh as output
+# grep usr name
+# get all processes
+# grep $proc
+res=$(top -b -n1 | grep -P "$usr" | awk '{print $12}' | grep -Pw "$proc")
+
+# return exit value 0 if process not is running, else 1
+if [ -z "$res" ]; then
+    # echo "Process $proc is not running"
+    exit 0
+else
+    # echo "Process $proc is running"
+    exit 1
+fi
