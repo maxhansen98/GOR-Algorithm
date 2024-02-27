b5284265d0fb1d12819cbaf51ae3cbb1f69280b7
Malte Weyrich <malte.weyrich@hotmail.de>
2024-02-27 08:49:42 +0100

    realized pgrep exists

--- shell_jagt/isrunning.sh
+++ shell_jagt/isrunning.sh
@@ -9,7 +9,10 @@ usr=$2
 # get all processes
 # grep $proc
 # setting the delay to be very short 
-res=$(top -b -d0.1 -n10 | grep -Pw "$usr" | grep -Pw "$proc")
+# res=$(top -b -d0.1 -n10 | grep -Pw "$usr" | grep -Pw "$proc")
+
+# didn't know this existed :)
+res=$(pgrep -u $usr $proc)
 
 # return exit value 0 if process not is running, else 1
 if [ -z "$res" ]; then
