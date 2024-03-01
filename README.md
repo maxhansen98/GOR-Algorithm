3c46c82ecc3becb725b2abd4e450dc13828c2bb8
Malte Weyrich <malte.weyrich@hotmail.de>
2024-03-01 12:11:09 +0100

    init

--- README.md
+++
@@ -1,4 +0,0 @@
-## Contribution guide
-> [!NOTE]
-> - Für jede Aufgabe/ jedes Projekt wird ein eigener Branch erstellt.
-> - Merges sind einschränkt
---
+++ homstrad/extract_all.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+# Programmierpraktikum WS2023/2024
+# Uebungsblatt 2, Aufgabe 14 (HOMSTRAD)
+# Malte A. Weyrich
+
+def get_db(path_to_homstrad):
+
+
+if __name__ == "__main__":
+   get_db("./HOMSTRAD")
