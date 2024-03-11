9eeaf4f688a9ed14bcbbf76166be3752f9fdaa7f
Malte Weyrich <malte.weyrich@hotmail.de>
2024-03-11 17:36:26 +0100

    added write to file again, went missing some how??

--- GOR/src/TrainerMain.java
+++ GOR/src/TrainerMain.java
@@ -13,6 +13,7 @@ public class TrainerMain {
         if (method.equals("gor1")) {
             TrainerGOR1 trainer = new TrainerGOR1(db, 1);
             trainer.train(model);
+            trainer.getSearchWindow().writeToFile(model);
         } else if (method.equals("gor3")) {
             TrainerGOR3 trainerGOR3 = new TrainerGOR3(db, 3);
             trainerGOR3.train(model);
@@ -20,6 +21,7 @@ public class TrainerMain {
         } else if (method.equals("gor4")) {
             TrainerGOR4 trainerGOR4 = new TrainerGOR4(db, 4);
             trainerGOR4.train(model);
+            trainerGOR4.getSearchWindow().writeToFile(model);
         }
 
         // "/home/malte/projects/blockgruppe3/GOR/CB513DSSP.db"
